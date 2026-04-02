ecd17569bffdda836b31eb68f125fa7df8ca1f00
whostacking <144233917+whostacking@users.noreply.github.com>
2026-04-02 22:22:53 +0800

    Update lbcomp.js

--- functions/lbcomp.js
+++ functions/lbcomp.js
@@ -22,13 +22,13 @@ async function handleLbCompRequest(interaction, botToAuthorMap, pruneMap) {
         }
 
         // Messages are returned in reverse chronological order (newest first)
-        // We want them in chronological order to parse them correctly (1..24 then 25..46)
+        // in chronological order to parse them correctly (1..24 then 25..46)
         const sortedMessages = [...messages.values()].sort((a, b) => a.createdTimestamp - b.createdTimestamp);
 
         const combinedContent = sortedMessages.map(m => m.content).join("\n");
 
         // Regex to match [Username](RobloxLink)
-        // Format in messages: ## 1 - [Sneffles](https://www.roblox.com/users/419612905/profile)
+        // Format: 1. [Username](https://www.roblox.com/users/1/profile)
         const playerRegex = /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g;
         let match;
         const players = [];
@@ -44,7 +44,7 @@ async function handleLbCompRequest(interaction, botToAuthorMap, pruneMap) {
             return interaction.editReply({ content: "No players found in the latest messages." });
         }
 
-        let description = "## Competitive Leaderboard\n\n";
+        let description = "## Competitive leaderboard\n";
         players.forEach((player, index) => {
             description += `${index + 1}. [${player.name}](${player.link})\n`;
         });
@@ -52,7 +52,6 @@ async function handleLbCompRequest(interaction, botToAuthorMap, pruneMap) {
         const container = new ContainerBuilder();
         const section = new SectionBuilder();
         section.addTextDisplayComponents([new TextDisplayBuilder().setContent(description.slice(0, 4000))]);
-        // Optional: set a transparent thumbnail to match existing style if desired
         section.setThumbnailAccessory(thumbnail => thumbnail.setURL("https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png"));
 
         container.addSectionComponents(section);
